94ce751ff5eb753a9276ac9691978925fccb4483
David-Thapa <daybeat06@gmail.com>
2026-08-16 14:25:15 +0100

    added for rag

--- run_eval.sh
+++ run_eval.sh
@@ -4,3 +4,8 @@ python eval.py \
   --input_file ../retrieval_lm/eval_data/popqa_longtail_w_gs.jsonl \
   --eval_file ../data/popqa/output/crag_popqa.txt \
   --metric match --task popqa
+
+python eval.py \
+  --input_file ../retrieval_lm/eval_data/popqa_longtail_w_gs.jsonl \
+  --eval_file ../data/popqa/output/rag_popqa.txt \
+  --metric match --task popqa
