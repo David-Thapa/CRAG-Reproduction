9bab8585b59a4ddf8ba973555c3c6cc69f13a1f7
David-Thapa <daybeat06@gmail.com>
2026-08-16 21:52:48 +0100

    added inference code for pubq

---
+++ run_crag_inference_pubq.sh
@@ -0,0 +1,11 @@
+cd scripts
+python CRAG_Inference.py \
+--generator_path microsoft/Phi-3-mini-4k-instruct \
+--evaluator_path ../models/finetuned_t5_evaluator \
+--input_file ../data/pubqa/test_pubqa.txt \
+--output_file ../data/pubqa/output/crag_pubqa_c2.txt \
+--internal_knowledge_path ../data/pubqa/ref/correct \
+--external_knowledge_path ../data/pubqa/ref/incorrect \
+--combined_knowledge_path ../data/pubqa/ref/ambiguous \
+--task pubqa --method crag --device cuda:0 \
+--ndocs 10 --batch_size 8 --upper_threshold 0.592 --lower_threshold 0.995
